9b19b8e4f5ccf9113b9ceef3af5a4d08956637aa
Christian Nuss <cnuss@users.noreply.github.com>
2022-12-04 11:14:50 -0800

    scaffoldly/serverless-api-stage-iam/aws@1.0.8

--- main.tf
+++ main.tf
@@ -204,7 +204,7 @@ module "websocket" {
 
 module "iam" {
   source  = "scaffoldly/serverless-api-stage-iam/aws"
-  version = "1.0.7"
+  version = "1.0.8"
 
   repository_name = var.repository_name
   stage           = var.stage
